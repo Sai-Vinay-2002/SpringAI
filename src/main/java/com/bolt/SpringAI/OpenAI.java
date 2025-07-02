7fa9e37c10f32762269eb8544c3d85c4b4c2bee5
Sai-Vinay-2002 <adkisaivinay@gmail.com>
2025-07-02 14:03:45 +0530

    completed

--- src/main/java/com/bolt/SpringAI/OpenAI.java
+++ src/main/java/com/bolt/SpringAI/OpenAI.java
@@ -4,21 +4,18 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OpenAI {
 
-//    private OpenAiChatModel chatModel;
-//
-//    public OpenAI(OpenAiChatModel chatModel)
-//    {
-//        this.chatModel=chatModel;
-//    }
+    private OpenAiChatModel chatModel;
+
+    public OpenAI(OpenAiChatModel chatModel)
+    {
+        this.chatModel=chatModel;
+    }
 
     @RequestMapping("/")
     public String homepage()
@@ -26,14 +23,14 @@ public class OpenAI {
         return "index";
     }
 
-//    @GetMapping("/prompt/{message}")
-//    public ModelAndView promptAnswer(@PathVariable String message, ModelAndView mv)
-//    {
-//
-//        String response=chatModel.call(message);
-//
-//        mv.addObject("response",response);
-//        mv.setViewName("response");
-//        return mv;
-//    }
+    @GetMapping("/prompt")
+    public ModelAndView promptAnswer(@RequestParam String message, ModelAndView mv)
+    {
+
+        String response=chatModel.call(message);
+
+        mv.addObject("response",response);
+        mv.setViewName("index");
+        return mv;
+    }
 }
